64016c2de7936e99a40848fd58bf46bfcb53d76e
SilverFS <f.susetyo@outlook.com>
2022-01-10 11:01:34 +0100

    Added guest + box tests

---
+++ AlgoritmiekTests/Guests/GuestListTest.cs
@@ -0,0 +1,46 @@
+﻿using Algoritmiek;
+using AlgoritmiekTests.Mockups;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmiekTests
+{
+    [TestClass]
+    public class GuestListTest
+    {
+        [TestMethod]
+        public void ShouldGetAllUsersWhenFunctionProvides()
+        {
+            //Arrange
+            GuestMockup mockup = new GuestMockup();
+            List<Guest> list;
+            //Act
+            list = mockup.CreateGuests();
+            //Assert
+            Assert.IsNotNull(list);
+            Assert.AreNotEqual(1, list.Count);
+            Assert.AreEqual(mockup.guestCount, list.Count);
+
+        }
+
+        [TestMethod]
+        public void ShouldGetAllBoxesWhenFunctionProvides()
+        {
+            //Arrange
+            GuestMockup mockup = new GuestMockup();
+            List<Box> boxesList;
+            //Act
+            boxesList = mockup.CreateBoxes();
+
+
+            //Assert
+            Assert.IsNotNull(boxesList);
+            //Assert sub lists too
+
+        }
+    }
+}
---
+++ AlgoritmiekTests/Mockups/GuestMockup.cs
@@ -0,0 +1,75 @@
+﻿using Algoritmiek;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmiekTests.Mockups
+{
+    public class GuestMockup
+    {
+        static Random random = new Random();
+        public readonly int guestCount = 101;
+        public List<Guest> guestListTest = new List<Guest>();
+        static double trueProbability = 0.8;
+        public List<Guest> CreateGuests()
+        {
+
+
+            for (int i = 0; i < guestCount; i++)
+            {
+                guestListTest.Add(new Guest
+                {
+                    guest_id = i,
+                    OnTime = random.NextDouble() < trueProbability,
+                    IsAdult = Convert.ToBoolean(random.Next(2)),
+                    group_id = random.Next(0, 9),
+                });
+            }
+            return guestListTest;
+        }
+
+        public List<Box> CreateBoxes()
+        {
+            List<Box> boxList = new List<Box>();
+            // counts specified
+            int boxCount = 3; // = 3
+
+            // For the amount of boxes
+            for (int i = 1; i < boxCount; i++)
+            {
+                int seatCount = random.Next(3, 11);
+                List<Row> rowList = new List<Row>();
+
+                // For the amount of rows = 2             
+                for (int j = 1; j < 2; j++)
+                {
+                    List<Seat> seatList = new List<Seat>();
+                    // For the amount of seats
+                    for (int k = 1; k < seatCount; k++)
+                    {
+                        seatList.Add(new Seat
+                        {
+                            seat_id = k,
+                            row_id = j,
+                            box_id = i,
+                        });
+                    }
+                    rowList.Add(new Row
+                    {
+                        row_id = j,
+                        box_id = i,
+                        seatList = seatList,
+                    });
+                }
+                boxList.Add(new Box
+                {
+                    box_id = i,
+                    rowList = rowList,
+                });
+            }
+            return boxList;
+        }
+    }
+}
