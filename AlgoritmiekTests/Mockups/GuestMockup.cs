335e9ab21c64fd831c150b13aa4497ba781b598e
SilverFS <f.susetyo@outlook.com>
2022-01-12 00:23:01 +0100

    Amplified projectTests

--- AlgoritmiekTests/Mockups/GuestMockup.cs
+++ AlgoritmiekTests/Mockups/GuestMockup.cs
@@ -1,4 +1,4 @@
-﻿using Algoritmiek;
+﻿using Algoritmiek.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,48 +28,6 @@ namespace AlgoritmiekTests.Mockups
                 });
             }
             return guestListTest;
-        }
-
-        public List<Box> CreateBoxes()
-        {
-            List<Box> boxList = new List<Box>();
-            // counts specified
-            int boxCount = 3; // = 3
-
-            // For the amount of boxes
-            for (int i = 1; i < boxCount; i++)
-            {
-                int seatCount = random.Next(3, 11);
-                List<Row> rowList = new List<Row>();
-
-                // For the amount of rows = 2             
-                for (int j = 1; j < 2; j++)
-                {
-                    List<Seat> seatList = new List<Seat>();
-                    // For the amount of seats
-                    for (int k = 1; k < seatCount; k++)
-                    {
-                        seatList.Add(new Seat
-                        {
-                            seat_id = k,
-                            row_id = j,
-                            box_id = i,
-                        });
-                    }
-                    rowList.Add(new Row
-                    {
-                        row_id = j,
-                        box_id = i,
-                        seatList = seatList,
-                    });
-                }
-                boxList.Add(new Box
-                {
-                    box_id = i,
-                    rowList = rowList,
-                });
-            }
-            return boxList;
         }
     }
 }
