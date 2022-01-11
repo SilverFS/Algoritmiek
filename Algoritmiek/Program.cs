d40dcfb27ec69f0d41197d041f77e87acc1f3713
SilverFS <f.susetyo@outlook.com>
2022-01-12 00:18:11 +0100

    Transfered files + restructure program to programcli

--- Algoritmiek/Program.cs
+++ Algoritmiek/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Algoritmiek.Models;
+using Algoritmiek.Containers;
 using System.Linq;
 using Spectre.Console;
 
@@ -7,42 +9,67 @@ namespace Algoritmiek
 {
 
     //Known issue: new menu entry gives new data.
-    public class Program
+    class Program
     {
         static Event greatEvent = new Event();
+        static GuestContainer guestContainer = new GuestContainer();
+        static BoxContainer boxContainer = new BoxContainer();
+
+
+
 
         //Menu loop
-        static void Main(string[] args)
+        static void MainOld(string[] args)
         {
+            boxContainer.CreateBoxes();
+            guestContainer.CreateGuestList();
+            //guestContainer.CreateFailedGuestList();
             bool showMenu = true;
             while (showMenu)
             {
                 showMenu = MainMenu();
             }
-
+            while (showMenu == false)
+            {
+                boxContainer.CreateBoxes();
+                guestContainer.CreateGuestList();
+                // guestContainer.CreateFailedGuestList();
+            }
         }
+
 
         //Menu definition
         private static bool MainMenu()
         {
-            var printGuests = greatEvent.CreateGuests();
+            var printGuests = guestContainer.CreateGuestList();
+            //var printGuests = guestContainer.CreateGuestList();
             Console.Clear();
             AnsiConsole.Markup("[bold green]Welcome to the event calculator![/] \n");
-            AnsiConsole.Markup("The total guests amount to: " + "[teal slowblink]" + greatEvent.guestCount + "[/]\n\n");
+            AnsiConsole.Markup("The total guests amount to: [teal slowblink]", printGuests.Count, "[/]\n\n");
             AnsiConsole.Markup("Choose an option:\n");
-            AnsiConsole.Markup("[yellow]1[/]) Displays a dummy table of random guests \n");
-            AnsiConsole.Markup("[yellow]2[/]) Generate event boxes\n");
-            AnsiConsole.Markup("\ntype '[red]exit[/]' to exit the program \n");
+            AnsiConsole.Markup("[yellow]1[/]) Generate event boxes\n");
+            AnsiConsole.Markup("[yellow]2[/]) Displays a dummy table of random guests \n");
+            AnsiConsole.Markup("[yellow]3[/]) Displays a dummy table of failed guests\n");
+            AnsiConsole.Markup("[yellow]4[/]) Display every option at once\n");
+            AnsiConsole.Markup("\nType '[red]exit[/]' to exit the program \n");
             AnsiConsole.Write("\r\nSelect an option: ");
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    ListOfGuests();
+                    ListOfBoxes();
                     Console.ReadLine();
                     return true;
                 case "2":
-                    ListOfBoxes();
+                    ListOfGuests();
+                    Console.ReadLine();
+                    return true;
+                case "3":
+                    //ListOfFailedGuests();
+                    Console.ReadLine();
+                    return true;
+                case "4":
+                    MainMenu();
                     Console.ReadLine();
                     return true;
                 case "exit":
@@ -53,15 +80,50 @@ namespace Algoritmiek
             }
         }
 
+
+
+
+        //Show failed guests with content
+        //private static void ListOfFailedGuests()
+        //{
+        //    var tableg = new Table();
+        //    tableg.Border(TableBorder.Simple);
+        //    tableg.BorderColor(Color.Orange3);
+        //    var printFailedGuests = guestContainer.CreateFailedGuestList();
+        //    var printGuests = guestContainer.CreateGuestList();
+
+        //    AnsiConsole.Markup("\nThe total guests amount to: " + "[teal slowblink]" + printGuests.Count + "[/]\n");
+        //    AnsiConsole.Markup("[bold]**The Total [green]passed[/] guests: [/][teal slowblink]" + printFailedGuests.Count + "[/]\n");
+        //    //Add columns
+        //    tableg.AddColumn("Guest");
+        //    tableg.AddColumn("On Time");
+        //    tableg.AddColumn("Is Adult");
+        //    tableg.AddColumn("Group Number");
+        //    foreach (var item in printFailedGuests)
+        //    {
+        //        //Add rows
+        //        tableg.AddRow("" + item.guest_id + "", "" + item.OnTime + "", "" + item.IsAdult + "", "" + item.group_id + "");
+        //    }
+        //    AnsiConsole.Write(tableg);
+        //    AnsiConsole.Markup("\n[italic]* This table is ordered by group id\n** This userlist is seperate data for boxes.\n   This means this table is completely irrelevant.[/]");
+        //    AnsiConsole.Markup("\nPress Enter to continue:");
+        //}
+
+
+
+
         //Show guests with content
-        private static void ListOfGuests()
+        public static void ListOfGuests()
         {
             var table = new Table();
             table.Border(TableBorder.Simple);
             table.BorderColor(Color.Orange3);
-            List<Guest> printGuests = greatEvent.CreateGuests();
-            AnsiConsole.Markup("\nThe total guests amount to: " + "[teal slowblink]" + greatEvent.guestCount + "[/]\n");
-            AnsiConsole.Markup("[bold]**The Total filtered guests: [/][teal slowblink]" + printGuests.Count + "[/]\n");
+            var printGuests = guestContainer.CreateGuestList();
+            //var printFailedGuests = guestContainer.CreateFailedGuestList();
+            //int passedGuests = Convert.ToInt32(printGuests.Count - printFailedGuests.Count);
+
+            AnsiConsole.Markup("\nThe total guests amount to: " + "[teal slowblink]" + printGuests.Count + "[/]\n");
+            //AnsiConsole.Markup("[bold]**The Total [green]passed[/] guests: [/][teal slowblink]" + passedGuests + "[/]\n");
             //Add columns
             table.AddColumn("Guest");
             table.AddColumn("On Time");
@@ -77,18 +139,20 @@ namespace Algoritmiek
             AnsiConsole.Markup("\nPress Enter to continue:");
         }
 
+
         //Show boxes with content
         private static void ListOfBoxes()
         {
             //Add necessary components
-            var printBoxes = greatEvent.CreateBoxes();
-            var printGuestList = greatEvent.CreateGuests();
+            var printBoxes = boxContainer.CreateBoxes();
+            var printGuests = guestContainer.CreateGuestList();
+
             //var boxesAndGuests = printBoxes.Zip(printGuestList, (b, g) => new { Boxes = b, Guests = g });
             //Add Tree root
             var root = new Tree("[red bold]Event[/]").Style("red");
 
             AnsiConsole.Markup("\n You have a total of: [teal slowblink]" + printBoxes.Count + "[/] boxes! \n");
-            AnsiConsole.Markup("\n You have a total of: [teal slowblink]" + printGuestList.Count + "[/] guests! \n");
+            AnsiConsole.Markup("\n You have a total of: [teal slowblink]" + printGuests.Count + "[/] guests! \n");
             AnsiConsole.Markup("_____________________\n\n");
             //Add nodes in loops
             foreach (var boxes in printBoxes)
@@ -99,7 +163,7 @@ namespace Algoritmiek
                     var singleRow = singleBox.AddNode("[blue bold]Row [/]" + rows.row_id);
                     foreach (var seats in rows.seatList)
                     {
-                        singleRow.AddNode("[lime bold]Seat [/]" + seats.seat_id + " -> guest: " + printGuestList.Count);
+                        singleRow.AddNode("[lime bold]Seat [/]" + seats.seat_id + " -> guest: " + printGuests.Count);
                     }
                 }
             }
---
+++ Algoritmiek/Programcli.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Algoritmiek.Models;
+using Algoritmiek.Containers;
+using System.Linq;
+using Spectre.Console;
+
+namespace Algoritmiek
+{
+
+    //Known issue: new menu entry gives new data.
+    class Programcli
+    {
+        static void Main(string[] args)
+        {
+            Event sortEvent = new Event();
+            BoxContainer boxContainer = new BoxContainer();
+            GroupContainer groupContainer = new GroupContainer();
+            GuestContainer guestContainer = new GuestContainer();
+            List<Box> boxList = boxContainer.CreateBoxes();
+            List<Guest> guestList = guestContainer.CreateGuestList();
+            List<Group> groupList = groupContainer.FormGroups(guestList);
+
+            Console.Title = "Event Calculator";
+            AnsiConsole.Markup($"Check for enough seats in comparison to guests: [orange3 slowblink]{ sortEvent.CheckRoomSeats() }[/]. " +
+                $"It counts [teal slowblink]{ sortEvent.CountAllSeats() }[/] seats.\n");
+            DisplayAll(boxList, groupList, guestList);
+            Console.WriteLine($"\nAll first seats in all rows: { sortEvent.CheckEmptyFirstRow() } \n");
+
+
+            Console.Read();
+        }
+
+
+        private static void DisplayAll(List<Box> boxList, List<Group> groupList, List<Guest> guestList)
+        {
+            AnsiConsole.Markup("[bold]**The Total [green]passed[/] guests: [/][teal slowblink]" + guestList.Count + "[/]\n");
+            AnsiConsole.Markup($"**The Total  groups amount to: {groupList.Count}\n\n");
+            AnsiConsole.Markup("\n[green bold underline]GUEST LIST[/]: \n");
+
+
+            var table = new Table();
+            table.Title("[red]All Guests[/]");
+
+
+            table.Border(TableBorder.Simple);
+            table.BorderColor(Color.Orange3);
+            //var printGuests = guestContainer.CreateGuestList();
+            int count = guestList.Count;
+            var last = guestList.FindLast;
+            //Add columns
+            table.AddColumn("Guest");
+            table.AddColumn("On Time");
+            table.AddColumn("Is Adult");
+            table.AddColumn("Group Number");
+            foreach (var item in guestList)
+            {
+                //Add rows
+                table.AddRow(item.guest_id + "", item.OnTime + "", item.IsAdult + "", item.group_id + "");
+            }
+
+            AnsiConsole.Write(table);
+            AnsiConsole.Markup("\n[italic]* This table is ordered by group id\n** This userlist is seperate data for boxes.\n   This means this table is completely irrelevant.[/]\n\n\n");
+
+            AnsiConsole.Markup("\n[green bold underline]GROUP LIST[/]: \n\n");
+            //Add Tree root for group list
+            var root2 = new Tree("[red bold]Event[/]").Style("red");
+
+
+            //Add columns
+            foreach (var item in groupList)
+            {
+                var singleGroup = root2.AddNode("[orangered1 bold]group id: [/]" + item.group_id + "");
+                foreach (var child in item.children)
+                {
+                    singleGroup.AddNode("[blue bold]Child ID: [/]" + child.guest_id + "");
+                }
+
+                foreach (var guest in item.guestsInGroup)
+                {
+                    singleGroup.AddNode("[blue bold]Adult ID: [/]" + guest.guest_id + "");
+                }
+            }
+            AnsiConsole.Write(root2);
+            AnsiConsole.Markup("\nBelow the box setup: \n");
+
+
+
+            AnsiConsole.Markup("\n[green bold underline]BOX LIST[/]: \n");
+            //Add Tree root
+            var root = new Tree("[red bold]Event[/]").Style("red");
+
+            AnsiConsole.Markup("\n You have a total of: [teal slowblink]" + boxList.Count + "[/] boxes! \n");
+            AnsiConsole.Markup("_____________________\n\n");
+            //Add nodes in loops
+            foreach (var boxes in boxList)
+            {
+                var singleBox = root.AddNode("[orangered1 bold]Box [/]" + boxes.box_id);
+                foreach (var rows in boxes.rowList)
+                {
+                    var singleRow = singleBox.AddNode("[blue bold]Row [/]" + rows.row_id);
+                    foreach (var seats in rows.seatList)
+                    {
+                        singleRow.AddNode("[lime bold]Seat [/]" + seats.seat_id + " -> guest: " + guestList.Count);
+                    }
+                }
+            }
+            AnsiConsole.Write(root);
+            AnsiConsole.Markup("\nPress Enter to continue:");
+        }
+    }
+}
