df1198fd263d37ebb184a8d0a9b75a0b5595a89a
SilverFS <f.susetyo@outlook.com>
2022-01-12 00:17:28 +0100

    Transfered models

---
+++ Algoritmiek/Models/Guest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmiek.Models
+{
+    public class Guest
+    {
+        public int guest_id { get; set; }
+        public bool OnTime { get; set; } = true;
+        public bool IsAdult { get; set; } = true;
+        public int group_id { get; set; }
+
+        public override string ToString()
+        {
+            return "Guest " + guest_id + ": " + "On Time: " + OnTime + " - " + "Is Adult: " + IsAdult + " - " + "Group ID: " + group_id;
+        }
+    }
+}
---
+++ Algoritmiek/Models/Seat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmiek.Models
+{
+    public class Seat
+    {
+        public int seat_id { get; set; }
+        public int row_id { get; set; }
+        public int box_id { get; set; }
+        public Guest guest { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return "Seat: " + seat_id + " | " + "Row: " + row_id + "Box: " + box_id + "Guest: " + guest;
+        //}
+    }
+}
