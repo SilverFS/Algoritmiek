c26e7ec501fe91f2ca013c00e49053b8d9080fff
SilverFS <f.susetyo@outlook.com>
2022-01-12 00:16:44 +0100

    Added container split + Groups

---
+++ Algoritmiek/Containers/BoxContainer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Algoritmiek.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmiek.Containers
+{
+    public class BoxContainer
+    {
+        // Defines Random
+        static Random random = new Random();
+
+        public List<Box> CreateBoxes()
+        {
+            List<Box> boxList = new List<Box>();
+            // counts specified
+            int boxCount = random.Next(3, 8);
+
+            // For the amount of boxes
+            for (int i = 1; i < boxCount; i++)
+            {
+                int seatCount = random.Next(3, 11);
+                List<Row> rowList = new List<Row>();
+
+                // For the amount of rows                
+                for (int j = 1; j < random.Next(2, 4); j++)
+                {
+                    List<Seat> seatList = new List<Seat>();
+                    // For the amount of seats
+                    for (int k = 1; k < seatCount; k++)
+                    {
+                        seatList.Add(new Seat
+                        {
+                            seat_id = k,
+                            row_id = j,
+                            box_id = i,
+                            //guest = guest_id,
+
+                        });
+                    }
+                    rowList.Add(new Row
+                    {
+                        row_id = j,
+                        box_id = i,
+                        seatList = seatList,
+                    });
+                }
+                boxList.Add(new Box
+                {
+                    box_id = i,
+                    rowList = rowList,
+                });
+            }
+            return boxList;
+        }
+    }
+}
---
+++ Algoritmiek/Containers/GroupContainer.cs
@@ -0,0 +1,38 @@
+﻿using Algoritmiek.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmiek.Containers
+{
+    public class GroupContainer
+    {
+
+        //needed: amount of groups, 
+        public List<Group> FormGroups(List<Guest> guestList)
+        {
+            List<Group> groups = new List<Group>();
+            int count = 0;
+            for (int i = 0; i < guestList.Count; i++)
+            {
+                //Adds users with group id to group until the next id
+                if (guestList[i].group_id == count)
+                {
+                    groups.Add(new Group
+                    {
+                        group_id = count,
+                        children = guestList.Where(x => x.group_id == count && x.IsAdult == false).ToList(),
+                        guestsInGroup = guestList.Where(x => x.group_id == count && x.IsAdult == true).ToList(),
+                    });
+                    count++;
+                }
+
+            }
+
+            groups = groups.OrderBy(x => x.group_id).ToList();
+            return groups;
+        }
+    }
+}
