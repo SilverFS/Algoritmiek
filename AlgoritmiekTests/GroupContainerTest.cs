839f5c57da3177800cb576ce622b42d202bf75db
SilverFS <f.susetyo@outlook.com>
2022-01-13 02:14:30 +0100

    Added GroupContainer Tests

---
+++ AlgoritmiekTests/GroupContainerTest.cs
@@ -0,0 +1,47 @@
+﻿using Algoritmiek.Containers;
+using Algoritmiek.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AlgoritmiekTests
+{
+    [TestClass]
+    public class GroupContainerTest
+    {
+        [TestMethod]
+        public void ShouldReturnGroupListWhenCalled()
+        {
+            //Arrange
+            GroupContainer groupContainer = new();
+            GuestContainer guestContainer = new();
+            List<Group> groupList;
+            List<Guest> guestList;
+            guestList = guestContainer.CreateGuestList();
+            //Act
+            groupList = groupContainer.FormGroups(guestList);
+            //Assert
+            Assert.IsNotNull(groupList);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProperGroupWhenCalled()
+        {
+            //Arrange
+            GroupContainer groupContainer = new();
+            GuestContainer guestContainer = new();
+            List<Group> groupList;
+            List<Guest> guestList;
+            guestList = guestContainer.CreateGuestList();
+            groupList = groupContainer.FormGroups(guestList);
+            int count = 0;
+            foreach (Group group in groupList)
+            {
+                count++;
+            }
+            //Act
+            //
+            //Assert
+            Assert.AreEqual(count, groupList.Count);
+        }
+    }
+}
