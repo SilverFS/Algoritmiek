fdff98aa42f678d537ce5eed916fb12dcf03fbc6
SilverFS <f.susetyo@outlook.com>
2022-01-13 02:04:42 +0100

    Added GuestContainer Tests

---
+++ AlgoritmiekTests/GuestContainerTest.cs
@@ -0,0 +1,46 @@
+﻿using Algoritmiek.Containers;
+using Algoritmiek.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AlgoritmiekTests
+{
+    [TestClass]
+    public class GuestContainerTest
+    {
+        [TestMethod]
+        public void ShouldReturnGuestListWhenCalled()
+        {
+            //Arrange
+            GuestContainer guestContainer = new();
+            List<Guest> guestList;
+            //Act
+            guestList = guestContainer.CreateGuestList();
+            //Assert
+            Assert.IsNotNull(guestList);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProperGuestWhenCalled()
+        {
+            //Arrange
+            GuestContainer guestContainer = new();
+            List<Guest> guestList;
+            //Act
+            guestList = guestContainer.CreateGuestList();
+            //Assert
+            Assert.AreEqual(0, guestList[0].group_id);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnOnTimeFalse()
+        {
+            GuestContainer guestContainer = new();
+            List<Guest> guestList;
+            //Act
+            guestList = guestContainer.CreateGuestList();
+            //Assert
+            Assert.AreNotEqual(false, guestList[0].OnTime);
+        }
+    }
+}
